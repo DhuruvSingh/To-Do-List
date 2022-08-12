9853a42f71cda5a8bf32f6b1da3c0dd22d708d65
Dhuruv Singh <84073153+DhuruvSingh@users.noreply.github.com>
2022-08-13 01:59:53 +0530

    Add function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,35 @@
-
 import './style.css';
 
-console.log('Hello World')
+const arrayList = [
+  {
+    description: 'Morning exercise',
+    complete: false,
+    index: 0,
+  },
+  {
+    description: 'Take a bath',
+    complete: false,
+    index: 1,
+  },
+  {
+    description: 'Have Breakfast',
+    complete: false,
+    index: 2,
+  },
+];
+
+function displayList() {
+  document.getElementById('ul-container-list').innerHTML = arrayList.map((items) => `<li id="listElement${items.index}"><input name="listElement${items.index}" type="checkbox"> ${items.description} <i class="fas fa-ellipsis-v"></i></li>`).join('');
+}
+displayList();
+
+document.getElementById('ul-container-list').addEventListener('click', (e) => {
+  document.getElementById(`${e.target.name}`).classList.toggle('text-crossLine');
+});
+
+document.getElementById('clearbtn-list').addEventListener('click', () => {
+  document.getElementById('ul-container-list').innerHTML = (
+    '<li class="testing-btn"> Testing delete list for 2 seconds </li>'
+  );
+  setInterval(displayList, 2000);
+});
