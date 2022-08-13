7cf34aac0549ffb3a6dd95d42481347ffd65a92c
Dhuruv Singh <84073153+DhuruvSingh@users.noreply.github.com>
2022-08-14 01:35:53 +0530

    fixed suggested feedback

--- src/popupOption.js
+++ src/popupOption.js
@@ -1,78 +1,79 @@
-import Crud from './add&delete.js';
-// eslint-disable-next-line
-import displayList from './index.js';
+// import Crud from './add&delete.js';
+// // eslint-disable-next-line
+// import displayList from './index.js';
 
-let firstTime = true;
-let changeArray = [];
+// let firstTime = true;
+// let changeArray = [];
 
-// eslint-disable-next-line
-export default class Modal {
-  static startModal(input) {
-    document.getElementById('delet-message-modal').innerHTML = 'To edit the task, just click on X and then click on the task';
-    document.getElementById('delet-Container-eachelement').classList.remove('display-none');
+// // eslint-disable-next-line
+// export default class Modal {
+//   static startModal(input) {
+// eslint-disable-next-line max-len
+//     document.getElementById('delet-message-modal').innerHTML = 'To edit the task, just click on X and then click on the task';
+//     document.getElementById('delet-Container-eachelement').classList.remove('display-none');
 
-    this.index = input;
+//     this.index = input;
 
-    const deleteBtn = document.getElementById('delet-btn-eachElement');
+//     const deleteBtn = document.getElementById('delet-btn-eachElement');
 
-    if (firstTime) {
-      firstTime = false;
-      deleteBtn.addEventListener('click', () => {
-        document.getElementById('ul-container-list').innerHTML = '';
-        Crud.delete(this.index);
-        document.getElementById('delet-Container-eachelement').classList.add('display-none');
-        displayList(Crud.add());
-        Crud.showChecked();
-      });
-    }
+//     if (firstTime) {
+//       firstTime = false;
+//       deleteBtn.addEventListener('click', () => {
+//         document.getElementById('ul-container-list').innerHTML = '';
+//         Crud.delete(this.index);
+//         document.getElementById('delet-Container-eachelement').classList.add('display-none');
+//         displayList(Crud.add());
+//         Crud.showChecked();
+//       });
+//     }
 
-    document.getElementById('delet-x-icon').addEventListener('click', this.closeModal);
-  }
+//     document.getElementById('delet-x-icon').addEventListener('click', this.closeModal);
+//   }
 
-  static closeModal() {
-    document.getElementById('delet-Container-eachelement').classList.add('display-none');
-    let newValue;
-    let path;
-    firstTime = true;
+//   static closeModal() {
+//     document.getElementById('delet-Container-eachelement').classList.add('display-none');
+//     let newValue;
+//     let path;
+//     firstTime = true;
 
-    document.getElementById('ul-container-list').addEventListener('click', (e) => {
-      const { id } = e.target;
-      const regex = /(?<=input)\d+$/;
+//     document.getElementById('ul-container-list').addEventListener('click', (e) => {
+//       const { id } = e.target;
+//       const regex = /(?<=input)\d+$/;
 
-      if (firstTime) {
-        if (regex.test(id)) {
-          firstTime = false;
-          const index = id.match(regex)[0];
-          path = document.getElementById(id);
-          path.disabled = false;
-          path.style.border = '1px solid black';
+//       if (firstTime) {
+//         if (regex.test(id)) {
+//           firstTime = false;
+//           const index = id.match(regex)[0];
+//           path = document.getElementById(id);
+//           path.disabled = false;
+//           path.style.border = '1px solid black';
 
-          path.addEventListener('change', () => {
-            newValue = document.getElementById(id).value;
-            path.disabled = 'disabled';
-            path.style.border = 'none';
-            Modal.changeValue(newValue, index);
-          });
-        } else {
-          firstTime = true;
-        }
-      }
-    });
-  }
+//           path.addEventListener('change', () => {
+//             newValue = document.getElementById(id).value;
+//             path.disabled = 'disabled';
+//             path.style.border = 'none';
+//             Modal.changeValue(newValue, index);
+//           });
+//         } else {
+//           firstTime = true;
+//         }
+//       }
+//     });
+//   }
 
-  static changeValue(newValue, position) {
-    changeArray = [];
-    if (newValue === '') return;
-    if (localStorage.getItem('data')) {
-      changeArray = JSON.parse(localStorage.getItem('data'));
-    }
+//   static changeValue(newValue, position) {
+//     changeArray = [];
+//     if (newValue === '') return;
+//     if (localStorage.getItem('data')) {
+//       changeArray = JSON.parse(localStorage.getItem('data'));
+//     }
 
-    for (let i = 0; i < changeArray.length; i += 1) {
-      // eslint-disable-next-line
-            if (changeArray[i].index == position) {
-        changeArray[i].description = newValue;
-        localStorage.setItem('data', JSON.stringify(changeArray));
-      }
-    }
-  }
-}
+//     for (let i = 0; i < changeArray.length; i += 1) {
+//       // eslint-disable-next-line
+//             if (changeArray[i].index == position) {
+//         changeArray[i].description = newValue;
+//         localStorage.setItem('data', JSON.stringify(changeArray));
+//       }
+//     }
+//   }
+// }
