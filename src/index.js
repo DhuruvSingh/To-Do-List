54ab2987fdc83db9b002cbd9feda54ae26196574
Dhuruv Singh <84073153+DhuruvSingh@users.noreply.github.com>
2022-08-14 00:29:52 +0530

    Made changes for some interective list

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 // import _ from 'lodash';
 // import { add } from 'lodash';
-import './style.css';
+import './styles.css';
+// eslint-disable-next-line
+import Checkbox from './checkbox.js';
 import Crud from './add&delete.js';
 // eslint-disable-next-line
 import Modal from './popupOption.js';
 
 export default function displayList(input) {
   document.getElementById('ul-container-list').innerHTML = input.map((items, index) => `
-  <li id="${index}"><input id="listElement${index}" type="checkbox"> 
+  <li id="${index}"><input id="listElement${index}" name='${index}' type="checkbox" value=${items.complete}> 
   <input id='input${index}' value="${items.description}" type="text" disabled="disabled" class='input-list'> 
   <i id="iconDots${index}" class="fas fa-ellipsis-v"></i></li>
   `).join('');
 }
 
-displayList(Crud.add());
+window.addEventListener('DOMContenLoaded', displayList(Crud.add()));
+window.addEventListener('DOMContenLoaded', Checkbox.testCheckbox());
 
 document.getElementById('input-list').addEventListener('change', () => {
   displayList(Crud.add());
@@ -23,6 +26,7 @@ const refreshIcons = document.getElementById('refresh-list');
 const updatingMessage = document.getElementById('updating-list-message');
 refreshIcons.addEventListener('click', () => {
   displayList(Crud.add());
+  Crud.showChecked();
   updatingMessage.classList.remove('display-none');
   refreshIcons.classList.add('fa-spin');
   setTimeout(() => {
@@ -46,3 +50,5 @@ document.getElementById('ul-container-list').addEventListener('click', (e) => {
     firstTime = true;
   }
 });
+
+document.getElementById('clearbtn-list').addEventListener('click', () => Checkbox.cleanAllChecked());
